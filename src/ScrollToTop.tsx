d683427b10a14cf89029741a37999102d3346e0d
miguelaraujo-tech <miguelaraujo@aesbvizela.pt>
2026-02-14 02:30:28 +0000

    Update ScrollToTop.tsx

--- src/ScrollToTop.tsx
+++ src/ScrollToTop.tsx
@@ -5,14 +5,7 @@ const ScrollToTop = () => {
   const location = useLocation();
 
   useLayoutEffect(() => {
-    // Force scroll on window
     window.scrollTo(0, 0);
-
-    // Also force scroll inside main container
-    const main = document.querySelector('main');
-    if (main) {
-      main.scrollTop = 0;
-    }
   }, [location.pathname]);
 
   return null;
