e8195a1e3ebced491ada1f97a63941bb3d190d7f
miguelaraujo-tech <miguelaraujo@aesbvizela.pt>
2026-02-14 00:18:25 +0000

    Update Logo.tsx try remove blac ring

--- Logo.tsx
+++ Logo.tsx
@@ -4,17 +4,25 @@ type LogoProps = {
 
 const Logo = ({ className = "" }: LogoProps) => {
   return (
-    <img
-      src="/REO_logo_round_512x512.png"
-      alt="REO – Rádio Escolar Online"
-      draggable={false}
-      className={`
-        object-contain
-        rounded-full
-        select-none
-        ${className}
-      `}
-    />
+    <div className="relative rounded-full overflow-hidden">
+      <img
+        src="/REO_logo_round_512x512.png"
+        alt="REO – Rádio Escolar Online"
+        draggable={false}
+        className={`
+          object-cover
+          rounded-full
+          select-none
+          scale-[1.07]
+          ${className}
+        `}
+        style={{
+          maskImage: "radial-gradient(circle at center, black 84%, transparent 85%)",
+          WebkitMaskImage:
+            "radial-gradient(circle at center, black 84%, transparent 85%)",
+        }}
+      />
+    </div>
   );
 };
 
