937db3313d163219cd85e54edcace627da328488
miguelaraujo-tech <miguelaraujo@aesbvizela.pt>
2026-02-14 00:22:18 +0000

    Update Logo.tsx

--- Logo.tsx
+++ Logo.tsx
@@ -4,25 +4,17 @@ type LogoProps = {
 
 const Logo = ({ className = "" }: LogoProps) => {
   return (
-    <div className="relative rounded-full overflow-hidden">
-      <img
-        src="/REO_logo_round_512x512.png"
-        alt="REO – Rádio Escolar Online"
-        draggable={false}
-        className={`
-          object-cover
-          rounded-full
-          select-none
-          scale-[1.07]
-          ${className}
-        `}
-        style={{
-          maskImage: "radial-gradient(circle at center, black 84%, transparent 85%)",
-          WebkitMaskImage:
-            "radial-gradient(circle at center, black 84%, transparent 85%)",
-        }}
-      />
-    </div>
+    <img
+      src="/REO_logo_round_512x512.png"
+      alt="REO – Rádio Escolar Online"
+      draggable={false}
+      className={`
+        object-contain
+        rounded-full
+        select-none
+        ${className}
+      `}
+    />
   );
 };
 
