32ec41d5b8d7b7d12d40ebc7bac486ba00aa33ac
miguelaraujo-tech <miguelaraujo@aesbvizela.pt>
2026-02-16 16:16:49 +0000

    Update App.tsx scrool to the top was not in src!!!

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ import Archive from './pages/Archive';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Logo from './Logo';
-import ScrollToTop from './ScrollToTop';
+import ScrollToTop from './src/ScrollToTop';
 
 /* ======================================================
    TYPE SAFE beforeinstallprompt
